069a3911023accbd62952dbe1fb7741375ce2d95
Jessica Haylay <hey@jayhay.me>
2016-04-03 09:10:02 -0400

    Add tests for dialogue callbacks

--- src/index.js
+++ src/index.js
@@ -23,9 +23,10 @@ class DialogueRunner {
      * @param {function} lineCallback - Callback function for when a line needs to be displayed
      * @param {function} optionsCallback - Callback function for when a choice must be made
      * @param {function} [startCallback] - Callback function for when dialogue is started
+     * @param {function} [finishCallback] - Callback function for when dialogue is finished
      * @param {function} [nodeCallback] - Callback function for after a node is finished
      */
-    constructor(lineCallback, optionsCallback, startCallback, nodeCallback) {
+    constructor(lineCallback, optionsCallback, startCallback, finishCallback, nodeCallback) {
         // Make sure we get functions
         if(typeof(lineCallback) !== 'function') {
             throw TypeError('Line callback must be a function!');
@@ -36,6 +37,9 @@ class DialogueRunner {
         if(startCallback && typeof(startCallback) !== 'function') {
             throw TypeError('If you have a start callback it must be a function!');
         }
+        if(finishCallback && typeof(finishCallback) !== 'function') {
+            throw TypeError('If you have a finish callback it must be a function!');
+        }
         if(nodeCallback && typeof(nodeCallback) !== 'function') {
             throw TypeError('If you have a node callback it must be a function!');
         }
@@ -43,6 +47,7 @@ class DialogueRunner {
         this.lineCallback = lineCallback;
         this.optionsCallback = optionsCallback;
         this.startCallback = startCallback;
+        this.finishCallback = finishCallback;
         this.nodeCallback = nodeCallback;
 
         this.dialogue = new Dialogue();
@@ -50,7 +55,7 @@ class DialogueRunner {
 
     /**
      * @param {string} startNode - Name of the node to start the dialogue from
-     * @return {Promise} - A promise to parse through and run the dialogue
+     * @return {Promise} A promise to parse through and run the dialogue
      */
     run(startNode) {
         return new Promise((resolve, reject) => {
@@ -69,13 +74,22 @@ class DialogueRunner {
                     // TODO: Command logic
                 }
                 else {
-                    throw new Error('Unrecognized dialogue result: ' + result);
+                    reject(new Error('Unrecognized dialogue result: ' + result));
                 }
             }
+
+            if(this.finishCallback) {
+                this.finishCallback();
+            }
+
+            resolve();
         });
     }
 }
 
 module.exports = {
     DialogueRunner: DialogueRunner,
+    LineResult: LineResult,
+    OptionsResult: OptionsResult,
+    CommandResult: CommandResult
 };
--- tests/tests.js
+++ tests/tests.js
@@ -2,21 +2,48 @@ var chai = require('chai'),
     expect = chai.expect,
     yarn = require('../src/index.js');
 
+const noop = () => {};
+
 describe('DialogueRunner', function() {
-    it('should let you create an instance with functions', function() {
-        var dialogue = new yarn.DialogueRunner(function() { }, function() { });
+    it('does let you create an instance with functions', function() {
+        var dialogue = new yarn.DialogueRunner(noop, noop);
 
         expect(dialogue.lineCallback).to.be.a('function');
         expect(dialogue.optionsCallback).to.be.a('function');
     });
-    it('should not let you create an empty instance of itself', function() {
+    it('does not let you create an empty instance of itself', function() {
         var createDialogue = () => { var dialogue = new yarn.DialogueRunner(); }
 
         expect(createDialogue).to.throw(TypeError);
     });
-    it('should not let you define non-function callbacks', function() {
+    it('does not let you define non-function callbacks', function() {
         var createDialogue = () => { var dialogue = new yarn.DialogueRunner(1, "hi"); }
 
         expect(createDialogue).to.throw(TypeError);
+    });
+
+    it('calls the line callback function if it gets a line result', function() {
+        var lineCallbackCalled = false;
+
+        var dialogue = new yarn.DialogueRunner(() => { lineCallbackCalled = true; },
+                                               noop);
+
+        dialogue.dialogue.run = () => [ new yarn.LineResult() ]
+
+        return dialogue.run().then(() => {
+            expect(lineCallbackCalled).to.be.true;
+        });
+    });
+    it('calls the options callback function if it gets an options result', function() {
+        var optionsCallbackCalled = false;
+
+        var dialogue = new yarn.DialogueRunner(noop,
+                                               () => { optionsCallbackCalled = true; });
+
+        dialogue.dialogue.run = () => [ new yarn.OptionsResult() ]
+
+        return dialogue.run().then(() => {
+            expect(optionsCallbackCalled).to.be.true;
+        });
     });
 });
