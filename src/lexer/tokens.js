488dba7bd1496f5fb1ece809c96dd57bdb4d039b
Todor Imreorov <blurymind@gmail.com>
2018-09-22 14:15:00 +0100

    allow even more symbols in cmdIdentifier

--- src/lexer/tokens.js
+++ src/lexer/tokens.js
@@ -89,7 +89,7 @@ const Tokens = {
 
   Identifier:           /[a-zA-Z0-9_:.]+/,         // a single word (used for functions)
 
-  CmdIdentifier:        /["'+=\[\]a-zA-Z0-9_:.,()]+/,  // a single word (used for commands)
+  CmdIdentifier:        /["'+-=\[\]\*a-zA-Z0-9_:;{}&|.,()]+/,  // a single word (used for commands)
 
   Text:                 /.*/,                      // a run of text until we hit other syntax.
 };
